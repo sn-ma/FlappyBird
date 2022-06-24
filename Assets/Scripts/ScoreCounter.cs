e9689a917791fc3525c436712c342f40aa6a39c2
Nikolay-Maryan Sokolovsky <n.m.sokolovsky@gmail.com>
2022-06-25 02:03:39 +0300

    Add score calculation

---
+++ Assets/Scripts/ScoreCounter.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FlappyBirdClone
+{
+    public class ScoreCounter : MonoBehaviour
+    {
+        [SerializeField]
+        private UnityEvent<int> onScoreChanged;
+
+        [SerializeField]
+        private Transform player;
+
+        public int score { get; private set; }
+
+        private LinkedList<float> scorePositions = new LinkedList<float>();
+
+        void Start()
+        {
+            ScoreMarker[] markers = GetComponentsInChildren<ScoreMarker>();
+            List<float> temp = new List<float>(markers.Length);
+            foreach (ScoreMarker marker in markers)
+            {
+                temp.Add(marker.transform.position.x);
+            }
+            temp.Sort();
+            foreach (float pos in temp)
+            {
+                scorePositions.AddLast(pos);
+            }
+        }
+
+        void Update()
+        {
+            while (player.position.x > scorePositions.First.Value)
+            {
+                IncScore();
+                scorePositions.RemoveFirst();
+            }
+        }
+
+        private void IncScore()
+        {
+            ++score;
+            onScoreChanged?.Invoke(score);
+            Debug.Log($"Score: {score}");
+        }
+
+        public void ResetScore()
+        {
+            score = 0;
+            onScoreChanged?.Invoke(score);
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreMarker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FlappyBirdClone
+{
+    public class ScoreMarker : MonoBehaviour
+    { }
+}
