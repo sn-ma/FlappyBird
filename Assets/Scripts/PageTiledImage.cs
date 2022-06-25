c4313b95eaf12518b88047c402bf1da8c64dd154
Nikolay-Maryan Sokolovsky <n.m.sokolovsky@gmail.com>
2022-06-25 06:11:21 +0300

    Reskin sky and ground

---
+++ Assets/Scripts/PageTiledImage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FlappyBirdClone
+{
+    public class PageTiledImage : MonoBehaviour
+    {
+        private float textureUnitSizeX;
+
+        void Start()
+        {
+            Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+            textureUnitSizeX = sprite.texture.width * transform.localScale.x / sprite.pixelsPerUnit;
+        }
+
+        void Update()
+        {
+            Vector3 position = transform.position;
+            float cameraPositionX = Camera.main.transform.position.x;
+            if (cameraPositionX - position.x > textureUnitSizeX)
+            {
+                position.x += textureUnitSizeX;
+            }
+            transform.position = position;
+        }
+    }
+}
